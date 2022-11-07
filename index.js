e2863e0a4a792e186990003b2814ebe55462998d
SaumyaRlogical <saumya.shah@rlogical.com>
2022-11-07 18:24:48 +0530

    minor fixed

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const connectToMongo = require("./db");
 const express = require("express");
 var cors = require("cors");
 
-const jwt = process.env.JWT_KEY;
-
 const app = express();
-const port = 6000;
+const port = process.env.PORT;
 connectToMongo();
 app.use(cors());
 app.use(express.json());
@@ -16,5 +14,4 @@ app.use("/api/project",require("./routes/project"))
 app.use("/api/timeEntry",require("./routes/timetable"))
 app.listen(port, () => {
   console.log(`Time-Entry Endpoints listening at http://localhost:${port}`);
-  console.log(jwt);
 });
