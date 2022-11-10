8ebaeaa1af68f5c85197c2a08c07c0883c8063b6
SaumyaRlogical <saumya.shah@rlogical.com>
2022-11-10 12:25:28 +0530

    completed timeentry system

--- models/TimeEntryEntity.js
+++ models/TimeEntryEntity.js
@@ -1,41 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const TimeEntrySchema = new Schema({
-  days: [
-    {
-      id: {
-        type: Schema.Types.ObjectId,
-      },
-      extraHours: {
-        type: Boolean,
-        default: false,
-      },
-      description: {
-        type: String,
-        required: true,
-      },
-      hours: {
-        type: Number,
-        required: true,
-      },
-      createdDate: {
-        type: Date,
-        default: Date.now,
-      },
-      project_id: [
-        {
-          type: mongoose.SchemaTypes.ObjectId,
-          ref: "project",
-          required: true,
-        },
-      ],
-      user_id: {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: "user",
-        required: true,
-      },
-    },
-  ],
+  extraHours: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  hours: {
+    type: Number,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+  project_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "project",
+    required: true,
+  },
+  user_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "user",
+    required: true,
+  },
 });
 const TimeTableEntity = mongoose.model("time-table", TimeEntrySchema);
 module.exports = TimeTableEntity;
